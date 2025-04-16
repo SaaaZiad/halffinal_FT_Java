95a8e43931ed95b4a56186f1ec4edc12551e3cff
Nikita Denisov <nikita97579@mail.ru>
2025-04-16 23:36:54 +0300

    Some fixes and changes

--- src/main/java/com/example/aviatickets/repositories/TicketRepo.java
+++ src/main/java/com/example/aviatickets/repositories/TicketRepo.java
@@ -12,14 +12,6 @@ import java.util.List;
 public interface TicketRepo extends JpaRepository<Ticket, Long>{
   @Query("select f from Ticket f where f.amount>0")
   List<Ticket> findAllAvailable();
-  @Query("SELECT p FROM Ticket p WHERE p.departure LIKE %?1%")
-  public List<Ticket> findByDeparture(String keywordDep);
-  @Query("SELECT z FROM Ticket z WHERE z.destination LIKE %?1%")
-  public List<Ticket> findByDestination(String keywordDest);
-  @Query("SELECT t FROM Ticket t WHERE t.strDepTime LIKE %?1%")
-  public List<Ticket> findByDepTime(String keywordDepTime);
-  @Query("SELECT r FROM Ticket r WHERE r.transportType LIKE %?1%")
-  public List<Ticket> findByTransport(String keywordTransport);
   @Query("SELECT r FROM Ticket r WHERE r.departure LIKE %?1% AND r.destination LIKE %?2% AND r.strDepTime LIKE %?3% AND r.transportType LIKE %?4%")
   public List<Ticket> findByFilters(String keywordDep, String keywordDest,
                                     String keywordDepTime, String keywordTransport);
